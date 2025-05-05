001cf4b3e3c406402826bbbe7b26d4a962bd8d6a
Phantom <mi95920@gmail.com>
2025-05-05 14:19:08 +0000

    Updates

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -7,6 +7,22 @@ const errHandlerMiddleware = (err, req, res, next) => {
         return res.status(err.statusCode).json({ msg: err.message })
     }
 
+    if (err.code && err.code === 11000) {
+        return res
+            .status(StatusCodes.FORBIDDEN)
+            .json({ msgS: `User with email: ${err.keyValue["email"]} already exist` })
+    }
+
+    if (err.name === "ValidationError") {
+        // const msge = Object.values(err.errors)
+        //     .map((item) => item.message)
+        //     .join(",")
+
+        return res
+            .status(StatusCodes.FORBIDDEN)
+            .json({ msgS: "Password can't be less than 6 Chars" })
+    }
+
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
         msg: "Something went wrong, pls try again later",
     })
